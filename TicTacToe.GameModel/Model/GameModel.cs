6107263e7c84ea364e1bc0ccba1c701196efab3e
Radchenko Artem <ddruming@yandex.ru>
2025-02-16 14:32:48 +0300

    style: Улучшено описание GameModel

--- TicTacToe.GameModel/Model/GameModel.cs
+++ TicTacToe.GameModel/Model/GameModel.cs
@@ -5,21 +5,29 @@ namespace TicTacToe.GameModel.Model;
 
 public class GameModel
 {
+    /*------------| < Private Fields > |------------*/
     private Board _board;
     private FieldItem _currentItem = Cross;
     private FieldItem _winner = Empty;
+    /*----------------------------------------------*/
 
+
+    /*--------------| < Properties > |--------------*/
     public FieldItem CurrentItem => _currentItem;
     public bool HasEmptyFields => _board.HasEmptyFields;
     public FieldItem Winner => _winner;
+    /*----------------------------------------------*/
 
 
+    /*--------------| < Constructors > |------------*/
     public GameModel(int size)
     {
         _board = new Board(size);
     }
+    /*----------------------------------------------*/
 
 
+    /*------------| < Public Methods > |------------*/
     public bool MakeMove(int row, int column)
     {
         if (_board.SetField(row, column, _currentItem))
@@ -38,4 +46,5 @@ public class GameModel
     }
 
     public void PrintBoard() => _board.PrintBoard();
+    /*----------------------------------------------*/
 }
