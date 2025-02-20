be064bc4979298ef539b726c619f092caef44950
Radchenko Artem <ddruming@yandex.ru>
2025-02-20 20:01:45 +0300

    feat: Внесены изменения в логику и отображение.
    Отредактирован index.html, добавлен скрипт в script.js а также подправлены стили в style.css

--- TicTacToe.View/Web/JavaScript/script.js
+++ TicTacToe.View/Web/JavaScript/script.js
@@ -1,2 +1,44 @@
 "use strict"
 
+document.addEventListener("DOMContentLoaded", () => {
+    const board = document.getElementById("board");
+    const fields = document.querySelectorAll(".cell");
+
+    fields.forEach((cell) => {
+        cell.addEventListener("click", async () => {
+            const row = parseInt(cell.dataset.row);
+            const col = parseInt(cell.dataset.col);
+
+            const response = await fetch("/api/game/move", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({row, col})
+            });
+
+            if (response.ok) {
+                const gameState = await response.json();
+
+                // Если сервер подтвердил ход, обновляем доску
+                if (gameState.moveMade) {
+                    updateBoard(gameState.board);
+                }
+            }
+        });
+    });
+
+
+    function updateBoard(boardState) {
+        boardState.forEach((row, rowIndex) => {
+            row.forEach((cellValue, colIndex) => {
+                const cell = document.querySelector(
+                    `.cell[data-row="${rowIndex}"][data-col="${colIndex}"]`
+                );
+
+                if (cellValue === 1) cell.textContent = "X";
+                else if (cellValue === 2) cell.textContent = "O";
+                else cell.textContent = "";
+
+            });
+        });
+    }
+});
