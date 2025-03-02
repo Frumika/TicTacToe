490e3e11f873a6ff4621e0c23c044b9a5977dfd8
Radchenko Artem <ddruming@yandex.ru>
2025-03-02 15:19:28 +0300

    feat: Добавлена новая страница - signIn.html

---
+++ TicTacToe.View/Web/JavaScript/SignIn/togglePassword.js
@@ -0,0 +1,16 @@
+"use strict"
+
+document.getElementById("toggle-password").addEventListener("click", function () {
+    let passwordInput = document.getElementById("password");
+    let eyeIcon = document.getElementById("image-eye-closed");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.src = "../Sources/Svg/eye_open.svg";
+        eyeIcon.alt = "Hide";
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.src = "../Sources/Svg/eye_closed.svg";
+        eyeIcon.alt = "Show";
+    }
+});
