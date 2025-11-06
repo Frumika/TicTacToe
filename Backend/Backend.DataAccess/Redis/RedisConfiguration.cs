c6dc838e3920d304817d4765fd9a2863425a232c
Radchenko Artem <ddruming@yandex.ru>
2025-11-06 17:32:31 +0300

    chore: удален неиспользуемый класс RedisConfiguration

--- Backend/Backend.DataAccess/Redis/RedisConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Backend.DataAccess.Redis;
-
-public class RedisConfiguration
-{
-    public string GameSessions { get; set; } = string.Empty;
-    public string UserSessions { get; set; } = string.Empty;   
-}
